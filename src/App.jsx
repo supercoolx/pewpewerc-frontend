2e02ffcd9c55a0df5ab07b3bcc2d9c3b4a0bd98a
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-09-02 12:59:00 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
 
 	return (
-		<div className='flex flex-col items-center justify-center'>
-			<div className="mx-auto">
-				<img src="./imgs/Group-91-1-p-800.png" width={672} sizes='(max-width: 479px) 96vw, (max-width: 991px) 80vw, 671.9921875px' className='' alt="" />
+		<div className='flex flex-col items-center justify-center px-3'>
+			<div className='ease-linear animate-appear'>
+				<div className="mx-auto mt-20">
+					<img src="./imgs/Group-91-1-p-800.png" width={672} sizes='(max-width: 479px) 96vw, (max-width: 991px) 80vw, 671.9921875px' className='' alt="" />
+				</div>
+				<div className="flex flex-wrap justify-center gap-5 mx-auto mt-10">
+					<a className='transition-all duration-100 cursor-pointer hover:-translate-y-3' href="https://x.com/Pewpew_erc"><img src='./imgs/Group-85-1.svg' width={106} /></a>
+					<a className='transition-all duration-100 cursor-pointer hover:-translate-y-3' href="https://t.me/pewpewerc20"><img src='./imgs/Group-87-1.svg' width={106} /></a>
+					<a className='transition-all duration-100 cursor-pointer hover:-translate-y-3' href="https://www.dextools.io/app/en/ether/pair-explorer/0xa64e7090db45e2a6d04460369f5945e0c1c15ea5?t=1719407557229"><img src='./imgs/Group-90-1.svg' width={106} /></a>
+				</div>
 			</div>
-			<div className="flex flex-wrap justify-center gap-5 mx-auto mt-10">
-				<img src='./imgs/Group-85-1.svg' width={106} />
-				<img src='./imgs/Group-87-1.svg' width={106} />
-				<img src='./imgs/Group-90-1.svg' width={106} />
-			</div>
-			<div className='px-5 mx-auto mt-20'>
+			<div className='mx-auto mt-20 ease-linear animate-appear'>
 				<img src="./imgs/Group-98-p-1600.png" className='' alt="" />
 			</div>
-			<div className='px-5 mx-auto mt-20'>
+			<div className='mx-auto mt-20 ease-linear animate-appear'>
 				<img src="./imgs/Group-95-2-p-1600.png" className='' alt="" />
 			</div>
-			<div className='px-5 mx-auto mt-20'>
+			<div className='mx-auto mt-20 ease-linear animate-appear'>
 				<img src="./imgs/Group-101-p-1600.png" className='' alt="" />
 			</div>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        warning: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 }
+        },
+        appear: {
+          '0%': { opacity: 0, transform: 'translate3d(0px, 0px, 0px) scale3d(0.8, 0.8, 0.8) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)' },
+          '100%': { transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d', opacity: 1 }
+        }
+      },
+      animation: {
+        appear: 'appear 0.5s ease-in-out 1'
+      }
+    },
   },
   plugins: [],
 }
